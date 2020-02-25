c0e4f9b62ed7a47187fc39f22e1722b4e166dcda
Ralph Olazo <ralph.olazo@nowcom.com.ph>
2020-02-25 16:38:28 +0800

    Add progress report initial page 1

--- resources/views/pages/backend/reports/progressReport.blade.php
+++ resources/views/pages/backend/reports/progressReport.blade.php
@@ -1,22 +1,102 @@
 <style type="text/css">
-.tg  {border-collapse:collapse;border-spacing:0;margin:0px auto;}
-.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;}
-.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;}
-.tg .tg-baqh{text-align:center;vertical-align:top}
-.tg .tg-amwm{font-weight:bold;text-align:center;vertical-align:top}
-.tg .tg-0lax{text-align:left;vertical-align:top}
+table {border-collapse:collapse;border-spacing:0;}
+table td{font-family:Arial, sans-serif;font-size:14px;padding:5px 5px;overflow:hidden;word-break:normal;}
+
+.tbl {border-style:solid;border-width:0px;}
+.tbl-bordered {border-style:solid;border-width:1px;}
+.tbl-bordered td{border-style:solid;border-width:1px;border-color:black;}
+
+tr .text-center {text-align: center; vertical-align:top;}
+tr .text-left {text-align: left; vertical-align:top;}
+
 </style>
-<table class="tg">
+<table class="tbl">
   <tr>
-    <th class="tg-amwm">UCLA Children's Hospital</th>
+    <td class="text-center">UCLA Children's Hospital</td>
   </tr>
   <tr>
-    <td class="tg-baqh">Development-Behavioral Pediatrics - Intake Form</td>
+    <td class="text-center">Development-Behavioral Pediatrics - Intake Form</td>
   </tr>
   <tr>
-    <td class="tg-0lax">Dear Caregivers,</td>
+    <td class="text-left">Dear Caregivers,</td>
   </tr>
   <tr>
-    <td class="tg-0lax"><br>This document provides your assessment team at the Developmental-Behavioral Pediatric Program with Information<br>to ensure the most productive visit for your child. Please complete the Intake Form providing as much <br>information as possible and return in to our program coordinator, Rosaura Cueva, via email, fax or mail as as you<br>can, so we can schedule your visit.<br></td>
+    <td class="text-left">This document provides your assessment team at the Developmental-Behavioral Pediatric Program with information to ensure the most productive visit for you and your child. Please complete the Intake Form providing as much information possible and return it to our program coordinator. Rosaura Cueva. via email, fax or mail soon as you can. So we can schedule your visit.</td>
   </tr>
+  <tr>
+    <td class="text-left">Additionally, please send us copies of any developmental-behavioral information on your child, as well as asking any daycare providers, teachers, therapists or other relevant providers to fill out the Provider Questionnaire. These forms ideally would be send before the visit if possible. Please bring the originals to the visit.</td>
+  </tr>
+  <tr>
+    <td class="text-left">Email:</td>
+  </tr>
+  <tr>
+    <td class="text-left">Phone:</td>
+  </tr>
+  <tr>
+    <td class="text-left">Fax:</td>
+  </tr>
+  <tr>
+    <td class="text-left">Visit checklist:</td>
+  </tr>
+  <tr>
+    <td class="text-left">
+    <input type="checkbox"/>
+    Intake Form</td>
+  </tr>
+  <tr>
+    <td class="text-left">
+    <input type="checkbox"/>
+    Teacher/Provider Questionnaire (Please copy the form if your child has multiple providers)</td>
+  </tr>
+  <tr>
+    <td class="text-left">
+    <input type="checkbox"/>
+    Most recent Regional Center Evaluation / Treatment Plan if applicable</td>
+  </tr>
+  <tr>
+    <td class="text-left">
+    <input type="checkbox"/>
+    Reports (e.g. Speech-language, Occupational therapy, Psychological evaluation, Behavioral therapy) if applicable</td>
+  </tr>
+  <tr>
+    <td class="text-left">
+    <input type="checkbox"/>
+    Most recent Individual Education Plan (IEP) AND any School Assessment Reports</td>
+  </tr>
+  <tr>
+    <td class="text-left">
+    <input type="checkbox"/>
+    A recent video or DVD of your child is also useful in helping us to plan for your visit if you have one available</td>
+  </tr>
+</table>
+<table class="tbl-bordered">  
+  <tr>
+    <td class="text-left">Date Completed:</td>
+    <td class="text-left" colspan="2">Completed by:</td>
+  </tr>  
+  <tr>
+    <td class="text-left">Child's name:</td>
+    <td class="text-left"  colspan="2">UCLA ID#:</td>
+  </tr>  
+  <tr>
+    <td class="text-left">Date of Birth</td>
+    <td class="text-left">Age:</td>
+    <td class="text-left">Gender:</td>
+  </tr> 
+  <tr>
+    <td class="text-left" colspan="3">Best phone number to reach caregiver:<br/><br/></td>    
+  </tr> 
+  <tr>
+    <td class="text-left" colspan="3">1. What are your main concerns about your child's development and/or behavior?<br/><br/></td>    
+  </tr> 
+  <tr>
+    <td class="text-left" colspan="3">2. What age was your child when you first became concerned about his/her development or behavior?<br/><br/></td>    
+  </tr> 
+  <tr>
+    <td class="text-left" colspan="3">3. What do you hope to gain from this visit?<br/><br/></td>    
+  </tr> 
+  <tr>
+    <td class="text-left" colspan="3">4. Describe your child's special qualities and strengths: <br/><br/></td>    
+  </tr> 
+
 </table>
