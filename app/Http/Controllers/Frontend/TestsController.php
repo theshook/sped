5f3ef0aafb13d7bdc660784d34bb2bf1fb493908
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2020-03-05 08:25:59 +0800

    Test controller for frontend (client) --blank

---
+++ app/Http/Controllers/Frontend/TestsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TestsController extends Controller
+{
+    /**
+     * Return tests available to take for assessment
+     *
+     * 
+     */
+    public function index()
+    {
+        //
+    }
+}
