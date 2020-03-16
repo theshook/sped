78fc7c9d66b7b3003fc04bee87ffa66ea01b19c5
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2020-03-16 17:21:06 +0800

    View blade footer added margin-top (ui fix)

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="sticky-footer bg-white">
+<footer class="sticky-footer bg-white mt-5">
 	<div class="container my-auto">
 		<div class="copyright text-center my-auto">
 			<span>Copyright &copy; Special Education Assessment 2020</span>
