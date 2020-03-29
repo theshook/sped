7bfcfbc151b13abefca41c3cae0a1d6280e376f8
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2020-03-29 12:55:57 +0800

    Assessments pupil controller --blank

---
+++ app/Http/Controllers/API/Backend/AssessmentPupilsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\API\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AssessmentPupilsController extends Controller
+{
+  //
+}
