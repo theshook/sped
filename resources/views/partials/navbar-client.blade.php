34616f8b55972eb8ca1b71ff01574d2404580eec
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2020-04-09 23:40:19 +0800

    Client navbar update

--- resources/views/partials/navbar-client.blade.php
+++ resources/views/partials/navbar-client.blade.php
@@ -1,59 +1,73 @@
-<div class="top-nav">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-6 px-0">
-        <ul class="nav">
+<div class="fixed-top">
+  <div class="top-nav">
+    <p class="top-nav-text">We are still on beta version</p>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 px-0">
+          <ul class="nav">
+            <li class="nav-item">
+              <a href="#" class="nav-link disabled">We are still on beta version</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="col-lg-6 px-0">
+          <ul class="nav justify-content-end top-nav-links p-0">
+            <li class="nav-item">
+              <a href="#" class="nav-link">Help</a>
+            </li>
+
+            <li class="nav-item">
+              <a href="#" class="nav-link">Contact</a>
+            </li>
+
+            <li class="nav-item">
+              <a href="#" class="nav-link">Parent Login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <div class="container">
+      <a href="#" class="navbar-brand font-weight-bolder text-primary">UCLA</a>
+
+      <button type="button" class="navbar-toggler text-dark border-0" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="fa fa-stream"></span>
+      </button>
+
+      <div class="collapse navbar-collapse">
+        {{-- LEFT PANE NAV --}}
+        <ul class="navbar-nav mr-auto">
           <li class="nav-item">
-            <a href="#" class="nav-link disabled">We are still on beta version</a>
+            <a href="#" class="nav-link link-to-home">Home</a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link link-to-assessments">Assessments</a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link link-to-announcements">Announcements</a>
           </li>
         </ul>
-      </div>
 
-      <div class="col-lg-6 px-0">
-        <ul class="nav justify-content-end top-nav-links p-0">
+        {{-- RIGHT PANE NAV --}}
+        <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a href="#" class="nav-link">Help</a>
+            <a href="#" class="nav-link link-to-announcements">
+              <span class="text-muted mr-2">Haven't registered yet?</span>
+              Click here
+            </a>
           </li>
 
           <li class="nav-item">
-            <a href="#" class="nav-link">Contact</a>
-          </li>
-
-          <li class="nav-item">
-            <a href="#" class="nav-link">Parent Login</a>
+            <a href="#" class="nav-link nav-link-fill mx-0">Login</a>
           </li>
         </ul>
       </div>
     </div>
-  </div>
+  </nav>
 </div>
-
-<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-  <div class="container">
-    <a href="#" class="navbar-brand font-weight-bolder text-primary">UCLA</a>
-
-    <div class="collapse navbar-collapse">
-      {{-- LEFT PANE NAV --}}
-      <ul class="navbar-nav mr-ato">
-        <li class="nav-item">
-          <a href="#" class="nav-link link-to-home">Home</a>
-        </li>
-
-        <li class="nav-item">
-          <a href="#" class="nav-link link-to-assessments">Assessments</a>
-        </li>
-
-        <li class="nav-item">
-          <a href="#" class="nav-link link-to-announcements">Announcements</a>
-        </li>
-      </ul>
-
-      {{-- RIGHT PANE NAV --}}
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <a href="#" class="nav-link nav-link-fill mx-0">Login</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
