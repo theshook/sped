d46c40b014c87b28cf36fb92822a1225a9269fb0
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2020-04-10 04:11:19 +0800

    Parent pages controller update

--- app/Http/Controllers/Frontend/ParentPagesController.php
+++ app/Http/Controllers/Frontend/ParentPagesController.php
@@ -3,17 +3,21 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class ParentPagesController extends Controller
 {
+  public function login()
+  {
+    return view('pages.frontend.parents.login');
+  }
+
   public function intake_form()
   {
-    return view('pages.frontend.parents.intake-form');
+    return view('pages.frontend.parents.intake-form.intake-form');
   }
 
   public function intake_form_fillup()
   {
-    return view('pages.frontend.parents.intake-form-fillup');
+    return view('pages.frontend.parents.intake-form.intake-form-fillup');
   }
 }
