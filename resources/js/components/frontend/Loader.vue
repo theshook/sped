9a07c23af09b7305bbcb4b9b6211777f78b961cc
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2020-04-09 23:40:07 +0800

    Loader.vue (page loader component)

---
+++ resources/js/components/frontend/Loader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <section class="loader-container">
+      <div class="loader-content">
+        <b-spinner type="grow" variant="primary"></b-spinner>
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+export default {
+  name: "PageLoader"
+};
+</script>
