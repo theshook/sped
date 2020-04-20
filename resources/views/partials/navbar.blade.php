532f6501cf3b379dc4548f7e989b7b35a2741371
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2020-04-21 00:00:30 +0800

    admin navbar removed search input box

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,10 +1,11 @@
-<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow-sm">
+<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top">
 	<!-- Sidebar Toggle (Topbar) -->
 	<button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
 		<i class="fa fa-bars"></i>
-	</button>
+  </button>
+
 	<!-- Topbar Search -->
-	<form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+	{{-- <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
 		<div class="input-group">
 			<input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
 			<div class="input-group-append">
@@ -13,7 +14,7 @@
 				</button>
 			</div>
 		</div>
-	</form>
+	</form> --}}
 
 	<!-- Topbar Navbar -->
 	<ul class="navbar-nav ml-auto">
@@ -157,7 +158,7 @@
 			</form>
 		</li>
 
-	
+
 
 		{{-- <!-- Nav Item - User Information -->
 		<li class="nav-item dropdown no-arrow">
