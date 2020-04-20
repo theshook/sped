5340fe2e7ab9c523d3c3a7ce32995606a8f53d0f
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2020-04-20 23:59:44 +0800

    new admin dashboard blade file

---
+++ resources/views/pages/backend/admin/dashboard.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('title')
+<title>ADMIN::Dashboard</title>
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
