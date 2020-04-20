5b5a0ccff7e061451d310a5d29a0fb9cf559d166
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2020-04-20 23:58:39 +0800

    Route service provider - redirect to /dashboard after login

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,104 +7,104 @@ use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * This namespace is applied to your controller routes.
-     *
-     * In addition, it is set as the URL generator's root namespace.
-     *
-     * @var string
-     */
-    protected $namespace = 'App\Http\Controllers';
+  /**
+   * This namespace is applied to your controller routes.
+   *
+   * In addition, it is set as the URL generator's root namespace.
+   *
+   * @var string
+   */
+  protected $namespace = 'App\Http\Controllers';
 
-    /**
-     * The path to the "home" route for your application.
-     *
-     * @var string
-     */
-    public const HOME = '/home';
+  /**
+   * The path to the "home" route for your application.
+   *
+   * @var string
+   */
+  public const HOME = '/dashboard';
 
-    /**
-     * Define your route model bindings, pattern filters, etc.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
+  /**
+   * Define your route model bindings, pattern filters, etc.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    //
 
-        parent::boot();
-    }
+    parent::boot();
+  }
 
-    /**
-     * Define the routes for the application.
-     *
-     * @return void
-     */
-    public function map()
-    {
-        $this->mapApiRoutes();
+  /**
+   * Define the routes for the application.
+   *
+   * @return void
+   */
+  public function map()
+  {
+    $this->mapApiRoutes();
 
-        $this->mapWebRoutes();
+    $this->mapWebRoutes();
 
-        $this->mapFrontendWebRoutes();
+    $this->mapFrontendWebRoutes();
 
-        $this->mapBackendWebRoutes();
-    }
+    $this->mapBackendWebRoutes();
+  }
 
-    /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @return void
-     */
-    protected function mapWebRoutes()
-    {
-        Route::middleware('web')
-            ->namespace($this->namespace)
-            ->group(base_path('routes/web.php'));
-    }
+  /**
+   * Define the "web" routes for the application.
+   *
+   * These routes all receive session state, CSRF protection, etc.
+   *
+   * @return void
+   */
+  protected function mapWebRoutes()
+  {
+    Route::middleware('web')
+      ->namespace($this->namespace)
+      ->group(base_path('routes/web.php'));
+  }
 
-    /** FRONTEND ROUTES
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @return void
-     */
-    protected function mapFrontendWebRoutes()
-    {
-        Route::middleware('web')
-            ->namespace($this->namespace)
-            ->group(base_path('routes/frontend/routes.php'));
-    }
+  /** FRONTEND ROUTES
+   * Define the "web" routes for the application.
+   *
+   * These routes all receive session state, CSRF protection, etc.
+   *
+   * @return void
+   */
+  protected function mapFrontendWebRoutes()
+  {
+    Route::middleware('web')
+      ->namespace($this->namespace)
+      ->group(base_path('routes/frontend/routes.php'));
+  }
 
-    /** BACKEND ROUTES
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @return void
-     */
-    protected function mapBackendWebRoutes()
-    {
-        Route::middleware('web')
-            ->namespace($this->namespace)
-            ->group(base_path('routes/backend/routes.php'));
-    }
+  /** BACKEND ROUTES
+   * Define the "web" routes for the application.
+   *
+   * These routes all receive session state, CSRF protection, etc.
+   *
+   * @return void
+   */
+  protected function mapBackendWebRoutes()
+  {
+    Route::middleware('web')
+      ->namespace($this->namespace)
+      ->group(base_path('routes/backend/routes.php'));
+  }
 
-    /**
-     * Define the "api" routes for the application.
-     *
-     * These routes are typically stateless.
-     *
-     * @return void
-     */
-    protected function mapApiRoutes()
-    {
-        Route::prefix('api')
-            ->middleware('api')
-            ->namespace($this->namespace)
-            ->group(base_path('routes/api.php'));
-    }
+  /**
+   * Define the "api" routes for the application.
+   *
+   * These routes are typically stateless.
+   *
+   * @return void
+   */
+  protected function mapApiRoutes()
+  {
+    Route::prefix('api')
+      ->middleware('api')
+      ->namespace($this->namespace)
+      ->group(base_path('routes/api.php'));
+  }
 }
